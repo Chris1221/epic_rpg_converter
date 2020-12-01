b2c0c4242fb9d19f6c68cf63a47ce2dac3f572d4
Christopher Cole <ccole@well.ox.ac.uk>
2020-12-01 08:15:25 -0500

    Add deps

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ setup(name='epic_rpg_convert',
       author='Chris Cole',
       author_email='chris.c.1221@gmail.com',
       packages=['epic_rpg_convert'],
+      install_requires = [
+        'discord.py==1.5.1',
+        'numpy==1.18.1',
+        'python-dotenv',
+        'psycopg2>2.8',
+        'networkx>2.0'],
       entry_points={
           'console_scripts': [
               'convert_bot = epic_rpg_convert:run',
